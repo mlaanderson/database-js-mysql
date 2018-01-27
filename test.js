b17bedfff93ca06396c1cb18abf05dc8c523cb05
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-01-27 16:37:00 -0600

    Fixed reserved word as column name

--- test.js
+++ test.js
@@ -9,9 +9,9 @@ var Connection = MySQL.open({
 });
 
 var promises = [];
-promises.push(Connection.execute('DROP TABLE IF EXISTS test1; CREATE TABLE test1 (key varchar(255), value varchar(1024));').then(() => {
+promises.push(Connection.execute('DROP TABLE IF EXISTS test1; CREATE TABLE test1 (name varchar(255), value varchar(1024));').then(() => {
     promises.push(Connection.execute("INSERT INTO test1 VALUES('name', 'Michael Anderson');").then(() => {
-        promises.push(Connection.query("SELECT * FROM test1 WHERE key = 'name';").then(data => {
+        promises.push(Connection.query("SELECT * FROM test1 WHERE name = 'name';").then(data => {
             if (data.length != 1) {
                 console.error('Invalid data returned');
                 Connection.execute('DROP TABLE test1;').then(() => {
