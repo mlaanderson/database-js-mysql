5c8cda0416acd10a1cfe2d08238c68aef57e3f20
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-01-27 16:54:29 -0600

    Try #2

--- test.js
+++ test.js
@@ -9,17 +9,22 @@ var Connection = MySQL.open({
 });
 
 var promises = [];
-promises.push(Connection.execute('DROP TABLE IF EXISTS test1; CREATE TABLE test1 (fl_name varchar(32), fl_value varchar(32));').then(() => {
-    promises.push(Connection.execute("INSERT INTO test1 VALUES('name', 'Michael Anderson');").then(() => {
-        promises.push(Connection.query("SELECT * FROM test1 WHERE fl_name = 'name';").then(data => {
-            if (data.length != 1) {
-                console.error('Invalid data returned');
-                Connection.execute('DROP TABLE test1;').then(() => {
-                    Connection.close().then(() => {
-                        process.exit(1);
-                    });
-                })
-            }
+promises.push(Connection.execute('DROP TABLE IF EXISTS test1;').then(() => {
+    promises.push(Connection.execute('CREATE TABLE test1 (fl_name varchar(32), fl_value varchar(32));').then(() => {
+        promises.push(Connection.execute("INSERT INTO test1 VALUES('name', 'Michael Anderson');").then(() => {
+            promises.push(Connection.query("SELECT * FROM test1 WHERE fl_name = 'name';").then(data => {
+                if (data.length != 1) {
+                    console.error('Invalid data returned');
+                    Connection.execute('DROP TABLE test1;').then(() => {
+                        Connection.close().then(() => {
+                            process.exit(1);
+                        });
+                    })
+                }
+            }).catch(error => {
+                console.error(error);
+                process.exit(1);
+            }));
         }).catch(error => {
             console.error(error);
             process.exit(1);
